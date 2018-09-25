0e4d341b1034b0f77cac3ff5ce54ff407573b38c
Gracie <aguilargrac92@gmail.com>
2018-09-24 19:52:19 -0500

    .
    .

--- tranferBluetooth/app/src/main/java/com/agui/tranferbluetooth/DirectoryFileObserver.java
+++ tranferBluetooth/app/src/main/java/com/agui/tranferbluetooth/DirectoryFileObserver.java
@@ -4,11 +4,15 @@ package com.agui.tranferbluetooth;
 import android.content.ContentValues;
 
 
+import android.content.Intent;
 import android.os.FileObserver;
 import android.util.Log;
 
 import java.io.File;
+import java.net.URISyntaxException;
 import java.util.List;
+
+import static android.content.Intent.*;
 
 
 /**
@@ -48,9 +52,11 @@ public class DirectoryFileObserver extends FileObserver {
 //                val.put(BluetoothShare.URI,"content://storage/emulated/0/Documents");
 //                val.put(BluetoothShare.DESTINATION, "B8:1D:AA:02:DO:AD");
 //                val.put(BluetoothShare.DIRECTION, BluetoothShare.DIRECTION_OUTBOUND);
-                  resend.enableBluetooth();
-
-
+                try {
+                    resend.onActivityResult(300, 1, Intent.getIntent(absolutePath));
+                } catch (URISyntaxException e) {
+                    e.printStackTrace();
+                }
 
 
         }
